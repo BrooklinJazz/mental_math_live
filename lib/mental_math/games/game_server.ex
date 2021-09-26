c1334d2b5331cf2c4c2b564f5d5819a6cad9d36d
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-09-26 00:23:21 -0700

    shared state for question

--- lib/mental_math/games/game_server.ex
+++ lib/mental_math/games/game_server.ex
@@ -4,12 +4,15 @@ defmodule MentalMath.Games.GameServer do
   alias MentalMath.SingleDigitAddition
   # Callbacks
   @score_table :score_table
+  @trivia_table :trivia_table
 
   @impl true
   def init(_opts) do
-    with :undefined <- :ets.whereis(@score_table) do
+    with :undefined <- :ets.whereis(@score_table), :undefined <- :ets.whereis(@trivia_table) do
       :ets.new(@score_table, [:set, :named_table, :public])
+      :ets.new(@trivia_table, [:set, :named_table, :public])
       :ets.insert(@score_table, {"player 1", 0})
+      :ets.insert(@trivia_table, {"player 1", new_trivia()})
     end
 
     {:ok, %{current_trivia: new_trivia()}}
@@ -32,9 +35,10 @@ defmodule MentalMath.Games.GameServer do
 
   @impl true
   def handle_call({:answer, answer}, _from, state) do
-    with true <- answer == state.current_trivia.answer do
+    with true <- answer == current_answer() do
       prev_score = score("player 1")
       :ets.insert(@score_table, {"player 1", prev_score + 1})
+      :ets.insert(@trivia_table, {"player 1", new_trivia()})
 
       {:reply, true,
        %{
@@ -59,12 +63,22 @@ defmodule MentalMath.Games.GameServer do
     end
   end
 
-  def current_question(server) do
-    GenServer.call(server, :current_question)
+  def current_trivia() do
+    case :ets.lookup(@trivia_table, "player 1") |> IO.inspect(label: "Question") do
+      [{_, value}] ->
+        value
+
+      _ ->
+        {:error, :trivia_not_found}
+    end
   end
 
-  def current_trivia(server) do
-    GenServer.call(server, :current_trivia)
+  def current_question(_) do
+    current_trivia().question
+  end
+
+  def current_answer() do
+    current_trivia().answer
   end
 
   def answer(server, answer) do
